60ef92fd94a81620ad3a0cf3331d4ae7b04542cc
KrishivJ <75926502+KrishivJ@users.noreply.github.com>
2021-06-13 18:55:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function gotPoses(results)
 function draw() {
 background('#6C91C2');
 
-  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+  document.getElementById("font_size").innerHTML = "Font size of the text will be =" + difference +"px";
 textSize(difference);
 fill('#FFE787');
 text('Krishiv', 50, 400);
